a4de012bb1985ee02f4da98b4d3c043a2d078450
Matthew Chan <matt@themattchan.com>
2015-09-20 06:13:12 -0400

    solns

--- SudokuError.scala
+++ SudokuError.scala
@@ -7,6 +7,25 @@ object SudokuError {
 
   val nums = (1 to 9).toSet
 
+  /* [2,3]=2
+   * [6,7]=8
+   */
+  val testSud: Grid =
+    """6 3 2 7 8 1 9 4 5
+  |4 8 7 5 9 6 2 1 3
+  |5 1 9 3 4 3 8 7 6
+  |8 6 4 3 5 2 7 9 1
+  |7 5 1 9 6 8 3 2 4
+  |2 9 3 1 7 4 6 5 8
+  |9 4 5 6 3 7 1 1 2
+  |1 7 6 8 2 5 4 3 9
+  |3 2 8 4 1 9 5 6 7"""
+      .stripMargin
+      .split("\n").zipWithIndex
+      .map { case (line,row) => {
+        val row_idxs = (0 to 8).map((row,_))
+        line.split(" ").map(_.toInt).zip(row_idxs).toVector } }.toVector
+
   def readSudoku() : Grid =  {
     { for (r <- 0 to 8) yield
     { val row_idxs = (0 to 8).map((r,_))
